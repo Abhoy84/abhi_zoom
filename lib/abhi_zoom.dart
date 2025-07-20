574f8f2ad86c862355082a07e1c3e6e84618c4c8
sayakmishra123 <Abhoy84>
2025-07-20 16:20:51 +0530

    Clean up lints and add documentation to AbhiZoom

--- lib/abhi_zoom.dart
+++ lib/abhi_zoom.dart
@@ -1,18 +1,28 @@
-library abhi_zoom;
-
 import 'package:flutter/material.dart';
 
+/// A widget that enables zooming and panning for its [child] using [InteractiveViewer].
+///
+/// Typically used for zoomable images, maps, or custom widgets.
+/// Customize [minScale] and [maxScale] to control zoom limits.
 class AbhiZoom extends StatelessWidget {
+  /// The widget to apply zoom and pan gestures to.
   final Widget child;
-  final double maxScale;
+
+  /// The minimum zoom scale (default is 1.0).
   final double minScale;
 
+  /// The maximum zoom scale (default is 5.0).
+  final double maxScale;
+
+  /// Creates an [AbhiZoom] widget.
+  ///
+  /// The [child] is required. You can optionally configure [minScale] and [maxScale].
   const AbhiZoom({
-    Key? key,
+    super.key,
     required this.child,
-    this.maxScale = 5.0,
     this.minScale = 1.0,
-  }) : super(key: key);
+    this.maxScale = 5.0,
+  });
 
   @override
   Widget build(BuildContext context) {
